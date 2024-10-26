e09a158787504e68d5572ab62ada34b241590f49
amiparadis250 <pishimweaime7@gmail.com>
2024-10-26 11:09:40 +0200

    fixing images incositency

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -179,8 +179,8 @@ function FooterComp() {
                                         />
                                     </svg>
                                 </a>
-                                {/* <a
-                                    href="javascript:void(0)"
+                                 <a
+                                    href="https://www.youtube.com/channel/UC_48UMZAviVqFs64UX8HXPQ"
                                     className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke hover:border-primary hover:bg-primary hover:text-white sm:mr-4 lg:mr-3 xl:mr-4 text-gray-200"
                                 >
                                     <svg
@@ -193,9 +193,9 @@ function FooterComp() {
                                             d="M15.6645 1.88018C15.4839 1.13364 14.9419 0.552995 14.2452 0.359447C13.0065 6.59222e-08 8 0 8 0C8 0 2.99355 6.59222e-08 1.75484 0.359447C1.05806 0.552995 0.516129 1.13364 0.335484 1.88018C0 3.23502 0 6 0 6C0 6 0 8.79263 0.335484 10.1198C0.516129 10.8664 1.05806 11.447 1.75484 11.6406C2.99355 12 8 12 8 12C8 12 13.0065 12 14.2452 11.6406C14.9419 11.447 15.4839 10.8664 15.6645 10.1198C16 8.79263 16 6 16 6C16 6 16 3.23502 15.6645 1.88018ZM6.4 8.57143V3.42857L10.5548 6L6.4 8.57143Z"
                                         />
                                     </svg>
-                                </a> */}
+                                </a> 
                                 <a
-                                    href="https://www.linkedin.com"
+                                    href="https://www.linkedin.com/in/eco-mama-9212ab332/"
                                     className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke hover:border-primary hover:bg-primary hover:text-white sm:mr-4 lg:mr-3 xl:mr-4 text-gray-200"
                                 >
                                     <svg
